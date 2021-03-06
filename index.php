478af5b89a253c2be771504f5698c84afcfdd0cf
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-06 17:06:50 -0500

    up

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ $config = json_decode(file_get_contents('config.json'), true);
 if (!isset($_COOKIE['id']) or !isset($_COOKIE['guid']) or !isset($_COOKIE['session_id'])) {
     header('location: https://api.paos.io/authorize');
 } else{
-    if ($config['wifi'] == null) {
-        header('location: wifi.php');
-    } else {
+    if ($config['wifi'] == true) {
         header('location: home.php');
+    } else {
+        header('location: wifi.php');
     }
 }

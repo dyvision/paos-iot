ee22d95b00fd7d11192223bf6b204be6bf935040
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-06 16:28:41 -0500

    wait why

--- py/wifi.py
+++ py/wifi.py
@@ -7,16 +7,8 @@ from paos_iot import *
 
 wifi_cli = wifi()
 try:
-    wifi_file = open('../wifi.json','r')
-    if sys.argv[1] == 'resetwifi':
-        Exception('resetting')
-    else:
-        cache = json.loads(wifi_file)
-        wifi_cli.set_wifi(wifi_file['ssid'],wifi_file['password'])
+    str(sys.argv[1])
+    print(wifi_cli.set_wifi(sys.argv[1],sys.argv[2]))
 except:
-    try:
-        str(sys.argv[1])
-        print(wifi_cli.set_wifi(sys.argv[1],sys.argv[2]))
-    except:
-        print(wifi_cli.get())
+    print(wifi_cli.get())
             

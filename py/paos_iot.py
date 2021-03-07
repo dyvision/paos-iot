3655644f0cab75466084d00f876201d4fcba5f57
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-06 22:22:52 -0500

    there

--- py/paos_iot.py
+++ py/paos_iot.py
@@ -43,16 +43,17 @@ class audio:
     def get_device(self):
         device_list = []
         shelllist = os.popen('pacmd list sinks | grep -E "Ports|analog-ou|hdmi-ou"').read().splitlines()
-        active_device = shelllist[-1].split(':')
-        active_device = active_device[-1].strip()
         for device in shelllist:
             stringarray = []
             stringarray = device.strip().split(':')
             del(stringarray[-1])
-            device_list.append(stringarray[0])
+            if stringarray[0] == 'active port':
+                continue
+            else:
+                device_list.append(stringarray[0])
             
         del(device_list[-1])
-        result = {'current_device': active_device, 'available_devices':device_list}
+        result = json.dumps(device_list)
 
         return json.dumps(result)
 

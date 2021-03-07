1199310c552f1fbf5887ac0ba260c7f73f975235
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-07 11:25:41 -0500

    parsec

---
+++ api/parsec.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include('/var/www/html/paos-iot/lib/paos-iot.php');
+
+use paos_iot\parsec;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $post = json_decode(file_get_contents('php://input'), true);
+    if ($post == null) {
+        $post = $_POST;
+        $parsec = new parsec();
+        if (json_decode($parsec->launch($_COOKIE['session_id'], $post['peer_id']), true)) {
+            header('location: ../parsec.php');
+        } else {
+            header('location: ../parsec.php?error=1');
+        }
+    } else {
+        $parsec = new parsec();
+        if (json_decode($parsec->launch($post['session_id'], $post['peer_id']), true)) {
+            $response['message'] = 'connected';
+        } else {
+            $response['message'] = 'could not connect';
+        }
+        print_r(json_encode($response));
+    }
+}
---
+++ parsec.php
@@ -0,0 +1,28 @@
+<head>
+    <script src='lib/paos_iot.js'></script>
+</head>
+<h1>Launch Parsec</h1>
+<?php
+//ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL); 
+include('lib/paos-iot.php');
+
+use paos_iot\parsec;
+
+$parsec = new parsec();
+?>
+
+<form action='api/parsec.php' method='POST'>
+    <select disabled name='device'>
+        <?php
+
+
+        $list = json_decode($parsec->get($_COOKIE['id'],$_COOKIE['guid']), true);
+        foreach ($list as $device) {
+           echo "<option value='".$device['peer_id']."'>".$device['name']."</option>";
+        }
+
+
+        ?>
+    </select>
+    <button>Launch</button>
+</form>

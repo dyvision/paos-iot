9e590383e60673cdc904cd0f1f5aed9fb50b2afd
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-07 10:01:08 -0500

    try it from the top

--- lib/paos-iot.php
+++ lib/paos-iot.php
@@ -141,7 +141,7 @@ namespace paos_iot {
         }
         function set_device($device)
         {
-            shell_exec(python . ' ' . py_path . 'audio.py device ' . $device);
+            shell_exec('pacmd set-card-profile 0 ' . $device);
             return;
         }
         function save($device)

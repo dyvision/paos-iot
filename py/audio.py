dfcc223626d4c1bf8c2f0c87dfcc1459c58428dd
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-07 10:51:16 -0500

    ag

--- py/audio.py
+++ py/audio.py
@@ -8,4 +8,5 @@ try:
     else:
         print(audio_cli.set_device(sys.argv[2]))
 except:
-    print(audio_cli.get_device())   
+    #print(audio_cli.get_device())   
+    print(audio_cli.set_volume(sys.argv[2],sys.argv[3]))

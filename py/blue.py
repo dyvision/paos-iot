667fd90375f6442753f32696eeba5da93cb58e18
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-09 23:17:32 -0500

    current

--- py/blue.py
+++ py/blue.py
@@ -7,6 +7,8 @@ try:
     str(sys.argv[1])
     if str(sys.argv[1]) == 'connect':
         print(blue_cli.set(sys.argv[2]))
+    elif str(sys.argv[1]) == 'current':
+        print(blue_cli.current(sys.argv[1]))
     else:
         print(blue_cli.delete_device(sys.argv[2]))
 except:

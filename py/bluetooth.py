e7ad3c90da1a8d99520885c674483935cda079c0
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-09 21:52:06 -0500

    try this

--- py/bluetooth.py
+++ py/bluetooth.py
@@ -1,10 +1,14 @@
 import sys
-from paos_iot import blue
+import bluetooth
+#from paos_iot import blue
 
-blue_cli = blue()
-try:
-    str(sys.argv[1])
-    if str(sys.argv[1]) == 'volume':
-        print(blue_cli.get())
-except:
-    print(blue_cli.get())
+#blue_cli = blue()
+#try:
+#    str(sys.argv[1])
+#    if str(sys.argv[1]) == 'volume':
+#        print(blue_cli.get())
+#except:
+#    print(blue_cli.get())
+
+nearby_devices = bluetooth.discover_devices(True)
+print(nearby_devices)

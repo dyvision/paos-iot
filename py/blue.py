dc94be72b2cc193618ad7419d396a0561197b3ce
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-09 21:58:07 -0500

    try now

---
+++ py/blue.py
@@ -0,0 +1,11 @@
+import sys
+import bluetooth
+from paos_iot import blue
+
+blue_cli = blue()
+try:
+    str(sys.argv[1])
+    if str(sys.argv[1]) == 'volume':
+        print(blue_cli.get())
+except:
+    print(blue_cli.get())

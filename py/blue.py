b9cabfb53c8e7f5f1d5cd90541346f467d311388
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-09 22:27:46 -0500

    testing

--- py/blue.py
+++ py/blue.py
@@ -2,8 +2,6 @@ import sys
 import bluetooth
 #from paos_iot import blue
 
-device = '98:7A:14:B0:C9:C5'
-
 #blue_cli = blue()
 #try:
 #    str(sys.argv[1])
@@ -13,10 +11,22 @@ device = '98:7A:14:B0:C9:C5'
 #        print(blue_cli.delete_device(sys.argv[2]))
 #except:
 #    print(blue_cli.get())
+
+hostMACAddress = '98:7A:14:B0:C9:C5' 
+port = 3
+backlog = 1
+size = 1024
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.bind((hostMACAddress, port))
+s.listen(backlog)
 try:
-    s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    s.connect((device,1))
-except bluetooth.btcommon.BluetoothError as err:
-    # Error handler
-    print(err)
-    pass
+    client, clientInfo = s.accept()
+    while 1:
+        data = client.recv(size)
+        if data:
+            print(data)
+            client.send(data) # Echo back to client
+except: 
+    print("Closing socket")
+    client.close()
+    s.close()

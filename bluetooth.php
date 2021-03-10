0c5bbcf62e0ea9e66439262b40d8a5742667f012
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-09 23:30:56 -0500

    separate div

--- bluetooth.php
+++ bluetooth.php
@@ -8,35 +8,36 @@
         <div class='exit'><a href='home.php'>X</a></div>
         <div class='card'>
             <img width='100%' height='auto' src='style/connection.png'>
-            <div style='overflow-y:scroll;height:100%'class='card-body'>
+            <div class='card-body'>
                 <h1>Connect your bluetooth controller</h1>
-                <h2>Current Devices:</h2>
-                <?php
-                include('lib/paos-iot.php');
+                <div style='overflow-y:scroll;height:200px'>
+                    <h2>Current Devices:</h2>
+                    <?php
+                    include('lib/paos-iot.php');
 
-                use paos_iot\blue;
+                    use paos_iot\blue;
 
-                $blue = new blue();
-                $current = json_decode($blue->current(), true);
-                foreach ($current as $device) {
-                    echo "<span>".$device['name']."<a style='color:red' href='api/blue.php?mac=".$device['mac']."'> X</a></span>";
-                }
-                ?>
-
+                    $blue = new blue();
+                    $current = json_decode($blue->current(), true);
+                    foreach ($current as $device) {
+                        echo "<span>" . $device['name'] . "<a style='color:red' href='api/blue.php?mac=" . $device['mac'] . "'> X</a></span>";
+                    }
+                    ?>
+                </div>
             </div>
             <form action='api/blue.php' method='POST'>
                 <select name='device'>
-        <?php
+                    <?php
 
 
-        $list = json_decode($blue->get(), true);
-        foreach ($list as $device) {
-            echo "<option value='$device[0]'>$device[1]</option>";
-        }
+                    $list = json_decode($blue->get(), true);
+                    foreach ($list as $device) {
+                        echo "<option value='$device[0]'>$device[1]</option>";
+                    }
 
 
-        ?>
-    </select>
+                    ?>
+                </select>
                 <button>Connect</button>
             </form>
         </div>

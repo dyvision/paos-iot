86e1f3e4928a53f46d3100162224a63e1ea4c414
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-11 00:39:36 -0500

    below

--- start.php
+++ start.php
@@ -23,7 +23,7 @@ include('lib/paos-iot.php');
                 if (json_decode($opcli->get(), true)['wifi'] == true) {
                 ?>
                     <span style='font-size: 1vw;'>Launch a Parsec session using your phone.</span>
-                    <span style='font-size:1vw;'>Scan the barcode above or navigate to https://api.paos.io to access the dashboard</span>
+                    <span style='font-size:1vw;'>Scan the barcode below or navigate to https://api.paos.io to access the dashboard</span>
                     <center>
                         <img style='margin:8px' width='25%' src='style/qr-code.png'>
                     </center>
@@ -31,7 +31,7 @@ include('lib/paos-iot.php');
                 } else {
                 ?>
                     <span style='font-size: 1vw;'>Configure your PAOS Device using your phone.</span>
-                    <span style='font-size:1vw;'>Scan the barcode above or navigate to https://api.paos.io to access the dashboard</span>
+                    <span style='font-size:1vw;'>Scan the barcode below or navigate to https://api.paos.io to access the dashboard</span>
                     <center>
                         <img style='margin:8px' width='25%' src='style/qr-code.png'>
                     </center>

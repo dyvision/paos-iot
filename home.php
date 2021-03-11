8ef04d9eb96e393a4fca0642913cab7ffb20986d
dyvision <dlang@dssinc.com>
2021-03-11 15:11:09 -0500

    display release

--- home.php
+++ home.php
@@ -11,11 +11,12 @@
     <center>
         <div class='card'>
             <img width='100%' height='auto' src='style/paos.png'>
-            <div class='card-body'>
+            <div style='overflow-y:scroll' class='card-body'>
                 <h1>Welcome to PAOS</h1>
                 <a class='menuitem' href='parsec.php'>Launch Parsec</a>
                 <a class='menuitem' href='wifi.php'>WiFi</a>
                 <a id='blue' class='menuitem' href='bluetooth.php' onclick='document.getElementById("blue").innerHTML = "Bluetooth - Scanning"'>Bluetooth</a>
+                <a class='menuitem' href='display.php'>Display</a>
                 <a class='menuitem' href='api/update.php'>Update</a>
             </div>
     </center>

4eabd9ebfef3ba79a013c1b105521f3b8b0f1167
dyvision <dlang@dssinc.com>
2021-03-11 15:51:20 -0500

    let's see

--- api/audio.php
+++ api/audio.php
@@ -1,5 +1,7 @@
 <?php
-ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL); 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include('/var/www/html/paos-iot/lib/paos-iot.php');
 
 use paos_iot\audio;
@@ -9,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($post == null) {
         $post = $_POST;
         $audio = new audio();
-        if (json_decode($audio->set_device($post['device']),true)) {
+        if (json_decode($audio->set_device($post['device']), true)) {
             #$audio->save($post['device']);
             header('location: ../audio.php');
         } else {
@@ -17,24 +19,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     } else {
         $audio = new audio();
-        if (json_decode($audio->set_device($post['device']),true)) {
+        if (json_decode($audio->set_device($post['device']), true)) {
             #$audio->save($post['device']);
-            $response['message'] = 'device changed to '.$post['device'];
+            $response['message'] = 'device changed to ' . $post['device'];
         } else {
             $response['message'] = 'could not connect';
         }
         print_r(json_encode($response));
     }
-} elseif($_SERVER['REQUEST_METHOD'] == 'PUT'){
+} elseif ($_SERVER['REQUEST_METHOD'] == 'PUT') {
     $post = json_decode(file_get_contents('php://input'), true);
     if ($post == null) {
         $post = $_POST;
     }
     $audio = new audio();
-        if (json_decode($audio->set_volume($post['direction'],$post['value']),true)) {
-            $response['message'] = 'success';
-        } else {
-            $response['message'] = 'failure';
-        }
-        print_r(json_encode($response));
+    $push = json_decode($audio->set_volume($post['direction'], $post['value']), true);
+    if ($push) {
+        $response['message'] = 'success';
+    } else {
+        $response['message'] = 'failure';
+    }
+    print_r(json_encode($response));
 }

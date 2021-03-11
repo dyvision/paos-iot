da67944ac861af6944f9ef1463cc4f52b26bd978
dyvision <dlang@dssinc.com>
2021-03-11 12:40:41 -0500

    try this

--- py/bluetooth.sh
+++ py/bluetooth.sh
@@ -12,14 +12,13 @@ send "scan on\r"
 send_user "\nSleeping\r"
 sleep 5
 send_user "\nDone sleeping\r"
-send "scan off\r"
 expect "Controller"
 send "trust $address\r"
 sleep 2
 send "pair $address\r"
 sleep 2
-send "0000\r"
-sleep 3
 send_user "\nShould be paired now.\r"
+send "scan off\r"
+sleep 1
 send "quit\r"
 expect eof

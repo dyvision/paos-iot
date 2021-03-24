6c523bfa5f10c3a1c97990a9285ecf4205d367cd
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-24 16:33:04 -0400

    right

--- py/audio.py
+++ py/audio.py
@@ -8,7 +8,7 @@ try:
         print(audio_cli.set_volume(sys.argv[2],sys.argv[3]))
     elif str(sys.argv[1] == 'scan'):
         print(audio_cli.get_device())
-    else:
-        print(audio_cli.set_device(sys.argv[3]))
+    elif str(sys.argv[1] == 'set'):
+        print(audio_cli.set_device(sys.argv[2]))
 except:
     print('error')

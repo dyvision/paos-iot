848af7a05d97e35209396815f2396a9b3e41a53d
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-03-24 16:58:49 -0400

    ' '

--- lib/paos-iot.php
+++ lib/paos-iot.php
@@ -163,7 +163,7 @@ namespace paos_iot {
         }
         function set_device($device)
         {
-                return exec('sudo -H -u parsec bash -c \'sudo ' . python . ' ' . py_path . 'audio.py set' . $device . '\'');
+                return exec('sudo -H -u parsec bash -c \'sudo ' . python . ' ' . py_path . 'audio.py set ' . $device . '\'');
         }
         function save($device)
         {
